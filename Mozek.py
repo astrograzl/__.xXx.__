122b9988a472eb0f7cc8bb98adb9a808ceac09c5
Zdeněk Janák <janak@astronomie.cz>
2017-08-23 09:35:56 +0200

    Me sexy brain de gusta

---
+++ Mozek.py
@@ -0,0 +1,34 @@
+#!python
+# coding: utf-8
+# @uthor: janak
+"""CNN model based on Keras for *parameters guesses
+    learned from POLLUX synthetic spectral database
+    trained goto LAMOST release 1 spectral database
+"""
+
+import sys
+import numpy as np
+from keras.models import load_model
+from tensorflow import device
+from castor import director
+
+
+try:
+    MODEL = load_model("lamost.h5")
+except (ImportError, ValueError) as _e_:
+    sys.exit(_e_)
+else:
+    MODEL.summary()
+
+
+with device('/gpu:0'):
+    HISTORY = MODEL.fit_generator(director("lamost"), 256, 1000,
+                                  validation_data=director("lamost"),
+                                  validation_steps=96,
+                                  verbose=0)
+
+MODEL.save("lamost.hdf5")
+np.savetxt("lamost.out", np.asarray([HISTORY.history["loss"],
+                                     HISTORY.history["acc"],
+                                     HISTORY.history["val_loss"],
+                                     HISTORY.history["val_acc"]]).T)
